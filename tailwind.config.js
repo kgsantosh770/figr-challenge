9ec41540df25b83b96ecb5fa757e7b52ac927438
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-11-16 19:28:08 +0530

    image and config extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'figr-blue': '#7053FF',
+        'figr-green': '#31b970',
+      },
+      backgroundImage:{
+        'dotted-pattern': "url('/src/assets/images/dotted-bg.png')",
+        'icon-pattern': "url('/src/assets/images/icon-bg.png')",
+        'item-pattern': "url('/src/assets/images/list-item-bg.png')",
+      },
+      transitionProperty: {
+        'spacing': 'margin, padding',
+        'position': 'top, left, bottom, right',
+      },
+      cursor:{
+        'figma-green': "url('/src/assets/images/cursor-green.png'), pointer", 
+      }
+    },
     fontFamily: {
       'inter': ['Inter', 'sans-serif'],
     }
