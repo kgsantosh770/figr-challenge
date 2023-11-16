256833b76047ed074ca4dd30939518675d0c0b31
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-11-16 21:57:14 +0530

    new color extended

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,9 @@ module.exports = {
       colors: {
         'figr-blue': '#7053FF',
         'figr-green': '#31b970',
+        'figr-text-blue': '#A594FD',
       },
-      backgroundImage:{
+      backgroundImage: {
         'dotted-pattern': "url('/src/assets/images/dotted-bg.png')",
         'icon-pattern': "url('/src/assets/images/icon-bg.png')",
         'item-pattern': "url('/src/assets/images/list-item-bg.png')",
@@ -16,8 +17,8 @@ module.exports = {
         'spacing': 'margin, padding',
         'position': 'top, left, bottom, right',
       },
-      cursor:{
-        'figma-green': "url('/src/assets/images/cursor-green.png'), pointer", 
+      cursor: {
+        'figma-green': "url('/src/assets/images/cursor-green.png'), pointer",
       }
     },
     fontFamily: {
