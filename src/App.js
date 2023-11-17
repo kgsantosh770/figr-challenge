58352eadbbe3e6f4434d62b96a0bc141e8d3ddbb
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-11-17 18:20:17 +0530

    components integrated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import BannerCard from "./components/banner-card/BannerCard";
 import Cards from "./components/cards/Cards";
+import MobileExperience from "./components/mobile-experience/MobileExperience";
 import Navbar from "./components/navbar/Navbar";
 import TextHighlighter from "./components/tex-highlighter/TextHighlighter";
 import Workflow from "./components/workflow/Workflow";
@@ -14,7 +15,7 @@ function App() {
       <Cards className="mt-40"/>
       <TextHighlighter/>
       <Workflow />
-
+      <MobileExperience />
     </div>
   );
 }
