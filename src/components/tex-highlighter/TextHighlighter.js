50f4e7f1e918981dbde5802c57bf93eadd28e6d7
Santosh <s.kunga.ganeshbabu@accenture.com>
2023-11-17 16:40:22 +0530

    text highlighting component developed

---
+++ src/components/tex-highlighter/TextHighlighter.js
@@ -0,0 +1,48 @@
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+const TextHighlighter = () => {
+    const textWrapper = useRef(null);
+    const text = "Jorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur tempus urna at turpis condimentum lobortis. Ut commodo efficitur neque.";
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+
+        let words = textWrapper.current.querySelectorAll('.word');
+        
+        const timeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: textWrapper.current,
+                start: "-12% top",
+                end: `+=${textWrapper.current.clientHeight - 90}px`,
+                pin: true,
+                scrub: true,
+            }
+        })
+
+        words.forEach((word, index) => {
+            timeline.to(word, {
+                color: 'white',
+                delay: 0,
+                stagger: 0.05,
+            });
+        });
+
+        return () => {
+            ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+        };
+    }, []);
+
+    return (
+        <div ref={textWrapper} className="mt-20">
+            <p className="text-[#202020] text-[2.6rem] font-bold w-[70%] mx-auto leading-snug">
+                {text.split(' ').map((word, index) => (
+                    <span key={index} className="word">{word} </span>
+                ))}
+            </p>
+        </div>
+    );
+};
+
+export default TextHighlighter;
